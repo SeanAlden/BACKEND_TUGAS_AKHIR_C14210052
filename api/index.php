17278ca9239a0f12fa5bfcc928b6525866a0b03c
Sean Alden <sa2596491@gmail.com>
2025-11-12 13:58:08 +0700

    fix index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,3 @@
-<!-- api/index.php -->
 <?php 
-
-// Atur konfigurasi PHP yang biasanya kamu tulis di php.ini
-ini_set('upload_max_filesize', '10M');
-ini_set('post_max_size', '10M');
-ini_set('memory_limit', '512M');
 
 require __DIR__. '/../public/index.php';
